3e5bdd398d6a1b86b8d3336034a9169c2e137176
LFK01 <37867917+LFK01@users.noreply.github.com>
2021-10-22 20:14:55 +0200

    Modified: - nominal_warehouse_dataset.py: new script to run simulations

--- src/Utils/data_utils/datasets_scripts/fault_warehouse_dataset.py
+++ src/Utils/data_utils/datasets_scripts/fault_warehouse_dataset.py
@@ -1,6 +1,5 @@
-import os
 import random
-from os import chdir
+from os import chdir, sep, getcwd
 from os.path import join
 import subprocess
 from xml.etree import ElementTree
@@ -10,6 +9,7 @@ from src.Utils.data_utils.datasets_scripts.nominal_warehouse_dataset import comp
 
 
 def modify_fault_warehouse_xsett_xml_file(par_element_tree: ElementTree.ElementTree,
+                                          sett_file_id: str,
                                           par_simulation_duration: int,
                                           par_seed: int):
     for element_iterator in par_element_tree.iter():
@@ -20,11 +20,13 @@ def modify_fault_warehouse_xsett_xml_file(par_element_tree: ElementTree.ElementT
         if element_iterator.tag == 'Seed':
             element_iterator.text = str(par_seed)
 
-    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files', 'Execution_fault_settings.xsett')
+    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files',
+                'Execution_fault_' + sett_file_id + '_settings.xsett')
     par_element_tree.write(path)
 
 
 def modify_fault_warehouse_xlayo_xml_file(par_element_tree: ElementTree.ElementTree,
+                                          layo_file_id: str,
                                           par_bot_count: int,
                                           par_vertical_aisles: int,
                                           par_horizontal_aisles: int,
@@ -42,33 +44,37 @@ def modify_fault_warehouse_xlayo_xml_file(par_element_tree: ElementTree.ElementT
         if element_iterator.tag == 'NReplenishmentStationWest':
             element_iterator.text = str(par_replenishment_stations)
 
-    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files', 'Execution_fault_layout.xlayo')
+    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files',
+                'Execution_fault_' + layo_file_id + '_layout.xlayo')
     par_element_tree.write(path)
 
 
-def modify_fault_warehouse_xconf_xml_file(par_element_tree: ElementTree.ElementTree):
-    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files',
-                'Execution_fault_configuration.xconf')
+def modify_fault_warehouse_xconf_xml_file(par_element_tree: ElementTree.ElementTree,
+                                          conf_file_id: str):
+    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files',
+                'Execution_fault_' + conf_file_id + '_configuration.xconf')
     par_element_tree.write(path)
 
 
-def create_fault_warehouse_csv_logs():
+def create_fault_warehouse_csv_logs(file_id: str,
+                                    idx_begin: int,
+                                    idx_end: int):
     # open xml file
-    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files', 'SimpleItem-Fill-200-200-4000.xsett')
+    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files',
+                'SimpleItem-Fill-200-200-8000.xsett')
     xsett_xml_file = ElementTree.parse(path)
 
-    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files', '1-4-4-32-0.85.xlayo')
+    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files',
+                '1-2-2-15-0.85.xlayo')
     xlayo_xml_file = ElementTree.parse(path)
 
-    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files',
-                'PPFAR-TABalanced-SAActivateAll-ISEmptiest-'
-                'PSNearest-RPDummy-OBPodMatching-RBSamePod-'
-                'MMNoChange.xconf')
+    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files',
+                'PPFAR-TABalanced-SAActivateAll-ISEmptiest-PSNearest-RPDummy-OBPodMatching-RBSamePod-MMNoChange.xconf')
     xconf_xml_file = ElementTree.parse(path)
 
-    xsett_filename = 'Execution_fault_settings.xsett'
-    xlayo_filename = 'Execution_fault_layout.xlayo'
-    xconf_filename = 'Execution_fault_configuration.xconf'
+    xsett_filename = 'Execution_fault_' + file_id + '_settings.xsett'
+    xlayo_filename = 'Execution_fault_' + file_id + '_layout.xlayo'
+    xconf_filename = 'Execution_fault_' + file_id + '_configuration.xconf'
     random.seed(Parser.read_seed())
 
     # variables to manage the simulation execution
@@ -76,16 +82,14 @@ def create_fault_warehouse_csv_logs():
 
     # variables to modify simulation parameters
     simulation_duration = 80000
-    aisles_number = [5, 6, 7]
+    aisles_number = [5, 6, 7, 8]
     initial_aisles_number = aisles_number[0] ** 2
-    initial_bot_number = 32
-    initial_pick_number = 2
-    initial_replenishment_number = 2
+    initial_bot_number = 15
     fault_modules = [20, 10, 5, 3]
     speed_operators = [2, 3]
 
     print('Doing Fault')
-    for horizontal_aisles_number in aisles_number:
+    for horizontal_aisles_number in aisles_number[idx_begin:idx_end]:
         for vertical_aisles_number in aisles_number:
             print('Doing Arena {}x{}'.format(horizontal_aisles_number, vertical_aisles_number))
 
@@ -94,8 +98,6 @@ def create_fault_warehouse_csv_logs():
                 par_vertical_aisles=vertical_aisles_number,
                 par_initial_aisles_number=initial_aisles_number,
                 par_initial_bot_number=initial_bot_number,
-                par_initial_pick_number=initial_pick_number,
-                par_initial_replenishment_number=initial_replenishment_number
             )
 
             seeds = random.sample(range(0, 1000), single_bot_fault_repetitions)
@@ -104,6 +106,7 @@ def create_fault_warehouse_csv_logs():
                 for speed_operator in speed_operators:
                     print('Doing single bot fault rep: {} out of {}'.format(i + 1, single_bot_fault_repetitions))
                     modify_fault_warehouse_xlayo_xml_file(par_element_tree=xlayo_xml_file,
+                                                          layo_file_id=file_id,
                                                           par_vertical_aisles=vertical_aisles_number,
                                                           par_horizontal_aisles=horizontal_aisles_number,
                                                           par_bot_count=current_bot_number,
@@ -111,23 +114,24 @@ def create_fault_warehouse_csv_logs():
                                                           par_replenishment_stations=current_replenishment_number)
 
                     modify_fault_warehouse_xsett_xml_file(par_element_tree=xsett_xml_file,
+                                                          sett_file_id=file_id,
                                                           par_simulation_duration=simulation_duration,
                                                           par_seed=seeds[i])
-                    modify_fault_warehouse_xconf_xml_file(par_element_tree=xconf_xml_file)
-                    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files')
+                    modify_fault_warehouse_xconf_xml_file(par_element_tree=xconf_xml_file,
+                                                          conf_file_id=file_id)
+                    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files')
                     chdir(path)
-                    exe_path = join('..', 'repos', 'RAWSim-O', 'RAWSimO.CLI', 'bin', 'x64',
+                    exe_path = join('..', 'RAWSim-O', 'RAWSimO.CLI', 'bin', 'x64',
                                     'Debug', 'RAWSimO.CLI.exe')
                     command_line = exe_path + ' ' \
                                             + xlayo_filename + ' ' \
                                             + xsett_filename + ' ' \
                                             + xconf_filename + ' ' \
-                                            + 'StatsDirFault 1'
+                                            + 'StatsDirFault' + file_id + '  1'
                     subproc = subprocess.Popen(command_line,
                                                stdin=subprocess.PIPE,
                                                shell=True,
                                                text=True)
-
                     module_offset = random.randint(0, 100)
                     subproc.communicate(input='N\n'
                                               '{}\n'.format(horizontal_aisles_number) +
@@ -143,6 +147,7 @@ def create_fault_warehouse_csv_logs():
                 for speed_operator in speed_operators:
                     print('Doing fault module: {} Speed operator: {}'.format(fault_module, speed_operator))
                     modify_fault_warehouse_xlayo_xml_file(par_element_tree=xlayo_xml_file,
+                                                          layo_file_id=file_id,
                                                           par_vertical_aisles=vertical_aisles_number,
                                                           par_horizontal_aisles=horizontal_aisles_number,
                                                           par_bot_count=current_bot_number,
@@ -150,18 +155,20 @@ def create_fault_warehouse_csv_logs():
                                                           par_replenishment_stations=current_replenishment_number)
 
                     modify_fault_warehouse_xsett_xml_file(par_element_tree=xsett_xml_file,
+                                                          sett_file_id=file_id,
                                                           par_simulation_duration=simulation_duration,
                                                           par_seed=seeds[counter])
-                    modify_fault_warehouse_xconf_xml_file(par_element_tree=xconf_xml_file)
-                    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files')
+                    modify_fault_warehouse_xconf_xml_file(par_element_tree=xconf_xml_file,
+                                                          conf_file_id=file_id)
+                    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files')
                     chdir(path)
-                    exe_path = join('..', 'repos', 'RAWSim-O', 'RAWSimO.CLI', 'bin', 'x64',
+                    exe_path = join('..', 'RAWSim-O', 'RAWSimO.CLI', 'bin', 'x64',
                                     'Debug', 'RAWSimO.CLI.exe')
                     command_line = exe_path + ' ' \
                                             + xlayo_filename + ' ' \
                                             + xsett_filename + ' ' \
                                             + xconf_filename + ' ' \
-                                            + 'StatsDirFault 1'
+                                            + 'StatsDirFault' + file_id + ' 1'
                     subproc = subprocess.Popen(command_line,
                                                stdin=subprocess.PIPE,
                                                shell=True,
@@ -179,4 +186,10 @@ def create_fault_warehouse_csv_logs():
 
 
 if __name__ == '__main__':
-    create_fault_warehouse_csv_logs()
+    # print(getcwd())
+    # path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files')
+    # print(path)
+    # chdir(path)
+    create_fault_warehouse_csv_logs(file_id='p0',
+                                    idx_begin=0,
+                                    idx_end=1)
--- src/Utils/data_utils/datasets_scripts/nominal_warehouse_dataset.py
+++ src/Utils/data_utils/datasets_scripts/nominal_warehouse_dataset.py
@@ -1,8 +1,8 @@
-import os
 import random
-from os import chdir
-from os.path import join
 import subprocess
+from os import chdir, sep
+from os.path import join
+from numpy import floor
 from xml.etree import ElementTree
 
 from src.Utils.Parser import Parser
@@ -20,7 +20,8 @@ def modify_warehouse_xsett_xml_file(par_element_tree: ElementTree.ElementTree,
         if element_iterator.tag == 'Seed':
             element_iterator.text = str(par_seed)
 
-    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files', 'Execution_settings.xsett')
+    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files',
+                'Execution_nominal_settings.xsett')
     par_element_tree.write(path)
 
 
@@ -42,57 +43,57 @@ def modify_warehouse_xlayo_xml_file(par_element_tree: ElementTree.ElementTree,
         if element_iterator.tag == 'NReplenishmentStationWest':
             element_iterator.text = str(par_replenishment_stations)
 
-    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files', 'Execution_layout.xlayo')
+    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files',
+                'Execution_nominal_layout.xlayo')
     par_element_tree.write(path)
 
 
 def modify_warehouse_xconf_xml_file(par_element_tree: ElementTree.ElementTree):
-    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files', 'Execution_configuration.xconf')
+    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files',
+                'Execution_nominal_configuration.xconf')
     par_element_tree.write(path)
 
 
 def compute_parameters(par_vertical_aisles: int,
                        par_horizontal_aisles: int,
                        par_initial_aisles_number: int,
-                       par_initial_bot_number: int,
-                       par_initial_pick_number: int,
-                       par_initial_replenishment_number: int, ):
+                       par_initial_bot_number: int):
     current_aisles_number = par_horizontal_aisles * par_vertical_aisles
     size_increase = current_aisles_number / par_initial_aisles_number
     current_bot_number = int(size_increase * par_initial_bot_number)
-    current_pick_number = int(size_increase * par_initial_pick_number)
-    current_replenishment_number = int(size_increase * par_initial_replenishment_number)
+    current_pick_number = int(floor(par_horizontal_aisles/2.0))
+    current_replenishment_number = current_pick_number
 
     return current_bot_number, current_pick_number, current_replenishment_number
 
 
 def create_nominal_warehouse_csv_logs():
     # open xml file
-    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files', 'SimpleItem-Fill-200-200-4000.xsett')
+    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files',
+                'SimpleItem-Fill-200-200-8000.xsett')
     xsett_xml_file = ElementTree.parse(path)
 
-    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files', '1-4-4-32-0.85.xlayo')
+    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files',
+                '1-2-2-15-0.85.xlayo')
     xlayo_xml_file = ElementTree.parse(path)
 
-    path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files',
+    path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files',
                 'PPFAR-TABalanced-SAActivateAll-ISEmptiest-PSNearest-RPDummy-OBPodMatching-RBSamePod-MMNoChange.xconf')
     xconf_xml_file = ElementTree.parse(path)
 
-    xsett_filename = 'Execution_settings.xsett'
-    xlayo_filename = 'Execution_layout.xlayo'
-    xconf_filename = 'Execution_configuration.xconf'
+    xsett_filename = 'Execution_nominal_settings.xsett'
+    xlayo_filename = 'Execution_nominal_layout.xlayo'
+    xconf_filename = 'Execution_nominal_configuration.xconf'
     random.seed(Parser.read_seed())
 
     # variables to manage the simulation execution
     nominal_exp_repetitions = 4
 
     # variables to modify simulation parameters
-    simulation_duration = 80000
-    aisles_number = [5, 6, 7]
+    simulation_duration = 8000
+    aisles_number = [5, 6, 7, 8]
     initial_aisles_number = aisles_number[0] ** 2
-    initial_bot_number = 32
-    initial_pick_number = 2
-    initial_replenishment_number = 2
+    initial_bot_number = 15
 
     print('Doing Nominal')
     for horizontal_aisles_number in aisles_number:
@@ -103,10 +104,7 @@ def create_nominal_warehouse_csv_logs():
                 par_horizontal_aisles=horizontal_aisles_number,
                 par_vertical_aisles=vertical_aisles_number,
                 par_initial_aisles_number=initial_aisles_number,
-                par_initial_bot_number=initial_bot_number,
-                par_initial_pick_number=initial_pick_number,
-                par_initial_replenishment_number=initial_replenishment_number
-            )
+                par_initial_bot_number=initial_bot_number)
 
             seeds = random.sample(range(0, 1000), nominal_exp_repetitions)
 
@@ -123,9 +121,9 @@ def create_nominal_warehouse_csv_logs():
                                                 par_simulation_duration=simulation_duration,
                                                 par_seed=seeds[i])
                 modify_warehouse_xconf_xml_file(par_element_tree=xconf_xml_file)
-                path = join('C:', os.sep, 'Users', 'Luciano', 'source', 'RawSim-O_conf_files')
+                path = join('C:', sep, 'Users', 'Luciano', 'source', 'repos', 'LFK01', 'Configuration_files')
                 chdir(path)
-                exe_path = join('..', 'repos', 'RAWSim-O', 'RAWSimO.CLI', 'bin', 'x64',
+                exe_path = join('..', 'RAWSim-O', 'RAWSimO.CLI', 'bin', 'x64',
                                 'Debug', 'RAWSimO.CLI.exe')
                 command_line = exe_path + ' ' \
                                         + xlayo_filename + ' ' \
@@ -143,4 +141,4 @@ def create_nominal_warehouse_csv_logs():
 
 
 if __name__ == '__main__':
-    execute_training_feature_set_datasets(task_name='WARE')
+    create_nominal_warehouse_csv_logs()
